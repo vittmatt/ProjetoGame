05eeee4b80f8803f922e35d2c5dfacdd494829bd
Vitor Almeida <vitor.fa2001@gmail.com>
2022-02-17 08:23:19 -0300

    Subindo branch

--- src/Console.java
+++ src/Console.java
@@ -1,5 +1,6 @@
 public interface Console {
-
+    // Adicionar exeptions para todos esses métodos//
+    // Adicionar Enum de desligar e ligar console//
     void joga();
     void trocaDeJogo();
     void desligar();
