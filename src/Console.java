90c36698148b341e6debfb8abf48dec941378c4c
Vitor Almeida <vitor.fa2001@gmail.com>
2022-02-17 11:42:59 -0300

    consertando comentário em console

--- src/Console.java
+++ src/Console.java
@@ -1,7 +1,7 @@
 public interface Console {
     // Adicionado Exceptions para Console e Jogador//
     // Adicionado Enums //
-    // Adicionar exeptions para todos esses métodos//
+    // Adicionado exeptions para todos esses métodos//
 
     void joga(boolean confirmacao) throws ConfirmacaoInvalida;
     void trocaDeJogo(String jogo) throws TrocaJogoInvalida;
