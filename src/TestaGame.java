9903666882f60fc1b7398c47fcf4b7772c262f90
Vitor Almeida <Vitor.fa2001@gmail.com>
2022-02-18 11:24:06 -0300

    consertando umas parada

--- src/TestaGame.java
+++ src/TestaGame.java
@@ -5,23 +5,23 @@ public class TestaGame {
             Console console = new PlayStation();
             PlayStation console2 = (PlayStation) console;
             Jogador j1 = null;//new Jogador("vitor", console2);
+            if (j1 == null) {
+                throw new JogadorNull("Jogador não pode ser null");
+            }
             j1.jogar(true);
 
             Console console3 = new Nintendo();
             Nintendo console4 = (Nintendo) console3;
             Jogador j2 = new Jogador("vitor", console4);
+            if (j2 == null) {
+                throw new JogadorNull("Jogador não pode ser null");
+            }
             console4.ligar();
             console4.baixarJogo();
             console4.baixarJogo();
             console4.trocaDeJogo("Dark souls");
             console4.desligar();
             console4.desligar();
-            if (j1 == null) {
-                throw new JogadorNull("Jogador não pode ser null");
-            }
-            if (j2 == null) {
-                throw new JogadorNull("Jogador não pode ser null");
-            }
         } catch (JogadorNull | JogadorSemConsole | ConfirmacaoInvalida |
                 TrocaJogoInvalida | ImpossivelDesligar | ImpossivelLigar e) {
             String msg = e.getMessage();
